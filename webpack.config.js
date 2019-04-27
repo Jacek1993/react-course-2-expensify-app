e2a8d8a6cf2dd0ffd0e7082c706a66e40c8c0344
Jacek1993 <jacek.rula1@gmail.com>
2019-04-27 22:38:42 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,85 @@
 //entry point  for us app.js
 //define output
 const path = require('path');
+const ExtractTextPlugin=require('extract-text-webpack-plugin');
 
+//funkcja moze miec dostep do zmiennej env podanej jako argument i to jest zaleta tego podejscia
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+
+    const isProduction=env==='production';
+    const CSSExtract=new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use:[
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options:{
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction? 'source-map':'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 };
+
+//source maps zajmuja glowna czesc miejsca to jest ten modul z devtool cheap-module-eval-source-map
+//source-map npm
+
+
+//
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//         path: path.join(__dirname, 'public'),
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [{
+//             loader: 'babel-loader',
+//             test: /\.js$/,
+//             exclude: /node_modules/
+//         }, {
+//             test: /\.s?css$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader'
+//             ]
+//         }]
+//     },
+//     devtool: 'cheap-module-eval-source-map',
+//     devServer: {
+//         contentBase: path.join(__dirname, 'public'),
+//         historyApiFallback: true
+//     }
+// };
